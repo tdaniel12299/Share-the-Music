a4be5d6e9a03cf16ec352e926ffcbb7f27deb553
Tyler D <130164977+tdaniel12299@users.noreply.github.com>
2024-05-05 05:12:45 -0400

    error in settings, installed apps, api.appS fixed

--- ShareTheMusic/api/urls.py
+++ ShareTheMusic/api/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path,  
+from django.urls import path  
 from .views import main
 
 urlpatterns = [
-    path('', main), #this links api url to the views.py which will run the main func in that app
+    path('', main) #this links api url to the views.py which will run the main func in that app
 ]
