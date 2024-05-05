2685239c43012180e62cf8efdb80f0bd1bf05082
Tyler D <130164977+tdaniel12299@users.noreply.github.com>
2024-05-05 05:52:26 -0400

    func for unique room code created in models,py

--- ShareTheMusic/api/models.py
+++ ShareTheMusic/api/models.py
@@ -1,4 +1,21 @@
 from django.db import models
+import string
+import random #these will be used to generate random codes for the rooms
+
+def uniqCode():
+    length = 6 
+
+    while True:
+        #random uses a func of choices which takes string and length module params (k), the string module
+        # has a method of ascii that allows us to select the type of chars we randomly generate
+        code = ''.join(random.choices(string.ascii_uppercase, k=length))
+
+        #checks to make sure code is unique, filters through the objects in Room targeting the code object
+        #in Room and comparing to the generated code, count method compares list
+        if Room.objects.filter(code=code).count() == 0:
+            break
+
+    return code
 
 # Create your models here.
 class Room(models.Model):
